7ee6d23c7f15a4baa59c2d541e8487e47eaabc8b
Ali Schueler <ali.schueler456@gmail.com>
2023-04-03 15:02:53 -0400

    this

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json());
 app.use(cors());
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb://127.0.0.1:27017/seniors'
+    || 'mongodb://127.0.0.1:27017/seniors';
 mongoose.connect(CONNECTION_STRING);
 app.get('/hello', (req, res) => {res.send('Hello World!')})
 CompletedController(app)
