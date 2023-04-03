3d057a91925c5e64a0d92f486592656fa905dd69
Ali Schueler <ali.schueler456@gmail.com>
2023-04-03 14:57:32 -0400

    cloud

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ import express from "express"
 import mongoose from "mongoose";
 import cors from 'cors'
 import CompletedController from "./controllers/completed/completed-controller.js";
-
 const app = express()
 app.use(express.json());
 app.use(cors());
-mongoose.connect('mongodb://127.0.0.1:27017/seniors');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://127.0.0.1:27017/seniors'
+mongoose.connect(CONNECTION_STRING);
 app.get('/hello', (req, res) => {res.send('Hello World!')})
 CompletedController(app)
 
