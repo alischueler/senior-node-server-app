c3412e5250cc9375636f309fab69a4807515f86d
Ali Schueler <ali.schueler456@gmail.com>
2023-04-04 14:31:54 -0400

    try

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ import CompletedController from "./controllers/completed/completed-controller.js
 const app = express()
 app.use(express.json());
 app.use(cors());
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb://127.0.0.1:27017/seniors';
+const CONNECTION_STRING ="mongodb+srv://aschueler:FineLine1@cluster0.hkdvw0r.mongodb.net/seniors";
 mongoose.connect(CONNECTION_STRING);
+console.log(CONNECTION_STRING)
 app.get('/hello', (req, res) => {res.send('Hello World!')})
 CompletedController(app)
 
